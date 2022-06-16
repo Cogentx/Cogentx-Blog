9db38e1a22e47b8a0e45cb3e4cee7cd194c7c4e1
Cogentx <code@cogentx.com>
2022-06-15 23:08:29 -0230

    Refactor to include Header/Footer on each page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import Layout from '../sections/Layout';
 
 // apply 'dark' class to trigger dark mode
 const Home: NextPage = () => {
-  return <Layout>Hello</Layout>;
+  return (<>
+    <Layout>Hello</Layout>
+  </>);
 };
 
 export default Home;
