218508c8c7947bfa0da662743e3b522041c6159e
Cogentx <code@cogentx.com>
2022-06-21 21:19:56 -0230

    Join Firestore to current user | custom react hook

--- src/lib/firebase/fb-hooks.ts
+++ src/lib/firebase/fb-hooks.ts
@@ -1,1 +1,30 @@
-export function useUserData() {}
+import { doc, getFirestore, onSnapshot } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './fb-init';
+
+export function useUserData() {
+  const [user] = useAuthState(auth);
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    // turn off realtime subscription
+    let unsubscribe;
+
+    if (user) {
+      const ref = doc(getFirestore(), 'users', user.uid);
+      unsubscribe = onSnapshot(ref, (doc) => {
+        setUsername(doc.data()?.username);
+      });
+    } else {
+      setUsername(null);
+    }
+
+    return unsubscribe;
+  }, [user]);
+
+  return {
+    user,
+    username,
+  };
+}
