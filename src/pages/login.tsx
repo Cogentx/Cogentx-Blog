7dd5be7aeee2e29c4d865378137063f1c4b41d23
Cogentx <code@cogentx.com>
2022-06-21 21:49:22 -0230

    Create skeleton signup page | link from login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import { useContext } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import Layout from '../sections/Layout';
 import Metatags from '../lib/nextjs/Metatags';
 import UsernameForm from '../components/UsernameForm';
@@ -17,7 +18,7 @@ import { UserContext } from '../lib/react/context';
 const LoginPage: NextPage = () => {
   const { user, username } = useContext(UserContext);
   console.log({user}, {username});
-  
+
 
   return (
     <>
@@ -34,11 +35,15 @@ const LoginPage: NextPage = () => {
               </button>
             )
           ) : (
-            <div>
+            <div className="flex flex-col items-center justify-center">
               <button className="accessBtn" onClick={fbSignInWithGoogle}>
                 <Image src="/google.png" alt="Google Logo" width={20} height={20} />
-                <span>Sign in with Google</span>
+                <span>Continue with Google</span>
               </button>
+
+              <div className="mt-20">
+                <Link href="/signup">Don&apos;t have an account? Sign Up →</Link>
+              </div>
             </div>
           )}
         </div>
