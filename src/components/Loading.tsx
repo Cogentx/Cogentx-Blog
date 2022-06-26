42b14bb1312dc11d1e47682a84d9043817715538
Cogentx <code@cogentx.com>
2022-06-26 15:02:35 -0230

    Re-do Loading Spinner

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,15 +1,10 @@
 // Loading Spinner
-import toast from 'react-hot-toast';
-import { Audio } from 'react-loader-spinner';
-
 type IProps = {
   show: boolean;
 };
 
 export default function Loading({ show }: IProps) {
-  return !show ? null : (
-    <div className="grid h-screen place-items-center" onClick={() => toast.success('Welcome to Cogent X Blog')}>
-      <Audio color="#01B400" height={100} width={100} />
-    </div>
-  );
+  return show ? (
+    <div className="w-14 h-14 border-[10px] border-indigo-200 border-t-indigo-500 rounded-full animate-loading-spinner"></div>
+  ) : null;
 }
