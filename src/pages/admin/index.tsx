07a2c354a89044339cb647da2771c16a5c9b8f1c
Cogentx <code@cogentx.com>
2022-06-28 12:52:05 -0230

    Create new post in Firestore with a custom slug ID

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,13 +1,18 @@
+import Metatags from '../../lib/nextjs/Metatags';
+import Layout from '../../sections/Layout';
 
 interface IProps {
   prop: any;
-};
+}
 
 export default function AdminPostsPage({ prop }: IProps) {
-
   return (
-    <main>
-      AdminPostsPage
-    </main>
+    <>
+      <Metatags title="Posts | Admin" description="Posts page with admin privileges" />
+
+      <Layout>
+        <div className="grid place-items-center">AdminPostsPage</div>
+      </Layout>
+    </>
   );
-};
+}
