e4b117bef4db4949b5045b5438973cf9a917a929
Cogentx <code@cogentx.com>
2022-06-28 15:15:27 -0230

    CreateNewPost component - 2

--- src/components/CreateNewPost.tsx
+++ src/components/CreateNewPost.tsx
@@ -1,16 +1,78 @@
-//TODO: factor out Firebase
-
-import { FormEventHandler } from 'react';
+import { FormEventHandler, useContext, useState } from 'react';
+import { doc, setDoc } from 'firebase/firestore';
+import kebabCase from 'lodash.kebabcase';
+import { db, auth } from '../lib/firebase/fb-init';
+import { UserContext } from '../lib/react/context';
+import { fbServerTimestamp } from '../lib/firebase/fb-firestore';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/router';
 
 /** Admin component to create new post
  *
  * @returns JSX.Element
  */
 export default function CreateNewPost() {
-  const createPost: FormEventHandler<HTMLFormElement> = (e) => {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+
+  const [title, setTitle] = useState('');
+
+  // Ensure slug is URL safe
+  const slug = encodeURI(kebabCase(title));
+
+  // Validate slug length
+  const isValid = title.length > 3 && title.length < 100;
+
+  //TODO: factor out Firebase
+  const createPost: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    console.log('Create New Post form submitted');
+
+    // Note: ManagePosts component must be wrapped by Auth Check to ensure user is logged in | otherwise 'useCollection' hook cannot be used. The '!' syntax in 'auth.currentUser!.uid' is our guarantee to TypeScript that 'auth.currentUser' will be available at runtime.
+    const uid = auth.currentUser!.uid;
+    const postRef = doc(db, 'users', uid, 'posts', slug);
+
+    // Advisable to provide all fields default values to avoid query errors later
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: '',
+      createdAt: fbServerTimestamp(),
+      updatedAt: fbServerTimestamp(),
+      heartCount: 0,
+    };
+
+    try {
+      await setDoc(postRef, data);
+
+      toast.success('Post created!');
+
+      // Imperative navigation after doc is set
+      router.push(`/admin/${slug}`);
+    } catch (error) {
+      //TODO: handle error properly
+      console.log('Error Creating New Post', error);
+    }
   };
 
-  return <form onSubmit={createPost}></form>;
+  return (
+    <form onSubmit={createPost}>
+      <input
+        type="text"
+        className="outline-none border-none text-4xl w-full py-1 px-3"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My Awesome New Article!"
+      />
+      <p>
+        <strong>Slug:</strong>
+        {slug}
+      </p>
+      <button type="submit" className="btn" disabled={!isValid}>
+        Create New Post
+      </button>
+    </form>
+  );
 }
